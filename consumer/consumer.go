abb393c8ebc230ce2401f60e412272849043ac3a
Chhaya Kumar Das <chhaya.das@verse.in>
2022-06-24 20:34:35 +0530

    Adding consumer

---
+++ consumer/consumer.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+
+	if len(os.Args) < 4 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <bootstrap-servers> <group> <topics..>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	bootstrapServers := os.Args[1]
+	group := os.Args[2]
+	topics := os.Args[3:]
+
+	sigchan := make(chan os.Signal)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":        bootstrapServers,
+		"broker.address.family":    "v4",
+		"group.id":                 group,
+		"session.timeout.ms":       6000,
+		"auto.offset.reset":        "earliest",
+		"enable.auto.offset.store": false,
+	})
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create a consumer: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Created Consumer %v\n", consumer)
+
+	err = consumer.SubscribeTopics(topics, nil)
+
+	for {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating", sig)
+			break
+		default:
+			event := consumer.Poll(100)
+			if event == nil {
+				continue
+			}
+
+			switch msg := event.(type) {
+			case *kafka.Message:
+				fmt.Printf("Message on %s:\n%s\n", msg.TopicPartition, msg.Value)
+				if msg.Headers != nil {
+					fmt.Printf("Headers: %v\n", msg.Headers)
+				}
+
+				_, err := consumer.StoreMessage(msg)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Error storing offset after message %s:\n", msg.TopicPartition)
+				}
+			case kafka.Error:
+				fmt.Fprintf(os.Stderr, "Error: %v: %v\n", msg.Code(), msg)
+				if msg.Code() == kafka.ErrAllBrokersDown {
+					break
+				}
+			default:
+				fmt.Printf("Ignored %v\n", msg)
+			}
+		}
+	}
+	fmt.Printf("Closing Consumer\n")
+	consumer.Close()
+}
